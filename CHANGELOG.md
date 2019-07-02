146efcae0b91924cd8c1d4b07ec0045e2fc4be9d
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-07-02 15:13:26 -0400

    needed to be cleaned

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,58 +4,20 @@ All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
 ## [4.0.2](https://github.com/embark-framework/embark/compare/v4.0.1...v4.0.2) (2019-04-11)
-<<<<<<< HEAD
 **Note:** Version bump only for package embark-dapp-template-demo
-=======
-
-**Note:** Version bump only for package embark-dapp-template-demo
-
-
-
-
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
 
 ## [4.0.1](https://github.com/embark-framework/embark/compare/v4.0.0...v4.0.1) (2019-03-26)
 
 **Note:** Version bump only for package embark-dapp-template-demo
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> resetProject
-
-
-
-
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
 
 # [4.0.0](https://github.com/embark-framework/embark/compare/v4.0.0-beta.2...v4.0.0) (2019-03-18)
 
 **Note:** Version bump only for package embark-dapp-template-demo
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> resetProject
-
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
-
-
 
 
 # [4.0.0-beta.2](https://github.com/embark-framework/embark/compare/v4.0.0-beta.1...v4.0.0-beta.2) (2019-03-18)
 
 **Note:** Version bump only for package embark-dapp-template-demo
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> resetProject
-
-
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
-
-
 
 # [4.0.0-beta.1](https://github.com/embark-framework/embark/compare/v4.0.0-beta.0...v4.0.0-beta.1) (2019-03-18)
 
@@ -63,15 +25,11 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 ### Features
 
 * add repository.directory field to package.json ([a9c5e1a](https://github.com/embark-framework/embark/commit/a9c5e1a))
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 * **@embark/pipeline:** Add `enabled` property to pipeline config ([5ea4807](https://github.com/embark-framework/embark/commit/5ea4807))
 
-=======
 * **@embark/pipeline:** Add `enabled` property to pipeline config ([5ea4807](https://github.com/embark-framework/embark/commit/5ea4807))
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
-=======
+
 * **@embark/pipeline:** Add `enabled` property to pipeline config ([5ea4807](https://github.com/embark-framework/embark/commit/5ea4807))
->>>>>>> resetProject
+
 * normalize README and package.json bugs, homepage, description ([5418f16](https://github.com/embark-framework/embark/commit/5418f16))
--- app/dapp.js
+++ app/dapp.js
@@ -1,114 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {Tabs, Tab} from 'react-bootstrap';
-
-import EmbarkJS from 'Embark/EmbarkJS';
-import Blockchain from './components/blockchain';
-import Whisper from './components/whisper';
-import Storage from './components/storage';
-import ENS from './components/ens';
-=======
-
-import EmbarkJS from 'Embark/EmbarkJS';
-
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
-=======
-
-import EmbarkJS from 'Embark/EmbarkJS';
-
->>>>>>> resetProject
+import EmbarkJS from 'Embark/EmbarkJS'; 
 
 import './dapp.css';
 
 class App extends React.Component {
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-  constructor(props) {
-    super(props);
-
-    this.handleSelect = this.handleSelect.bind(this);
-
-    this.state = {
-      error: null,
-      activeKey: 1,
-      whisperEnabled: false,
-      storageEnabled: false,
-      blockchainEnabled: false
-    };
-  }
-
-  componentDidMount() {
-    EmbarkJS.onReady((err) => {
-      this.setState({blockchainEnabled: true});
-      if (err) {
-        // If err is not null then it means something went wrong connecting to ethereum
-        // you can use this to ask the user to enable metamask for e.g
-        return this.setState({error: err.message || err});
-      }
-
-      EmbarkJS.Messages.Providers.whisper.getWhisperVersion((err, _version) => {
-        if (err) {
-          return console.log(err);
-        }
-        this.setState({whisperEnabled: true});
-      });
-
-      EmbarkJS.Storage.isAvailable().then((result) => {
-        this.setState({storageEnabled: result});
-      }).catch(() => {
-        this.setState({storageEnabled: false});
-      });
-    });
-  }
-
-  _renderStatus(title, available) {
-    let className = available ? 'pull-right status-online' : 'pull-right status-offline';
-    return <React.Fragment>
-      {title}
-      <span className={className}></span>
-    </React.Fragment>;
-  }
-
-  handleSelect(key) {
-    this.setState({ activeKey: key });
-  }
-=======
->>>>>>> resetProject
-
-  render() {
-    
-    return (<div>
-<<<<<<< HEAD
-      <h3>Embark - Usage Example</h3>
-      <Tabs onSelect={this.handleSelect} activeKey={this.state.activeKey} id="uncontrolled-tab-example">
-        <Tab eventKey={1} title={this._renderStatus('Blockchain', this.state.blockchainEnabled)}>
-          <Blockchain/>
-        </Tab>
-        <Tab eventKey={2} title={this._renderStatus('Decentralized Storage', this.state.storageEnabled)}>
-          <Storage enabled={this.state.storageEnabled}/>
-        </Tab>
-        <Tab eventKey={3} title={this._renderStatus('P2P communication (Whisper)', this.state.whisperEnabled)}>
-          <Whisper enabled={this.state.whisperEnabled}/>
-        </Tab>
-        <Tab eventKey={4} title={this._renderStatus('Naming (ENS)', ensEnabled)}>
-          <ENS enabled={ensEnabled}/>
-        </Tab>
-      </Tabs>
-=======
-
-  render() {
-    
+  render() { 
     return (<div>
       <h3>Yo Dima.</h3>
-      
->>>>>>> f17c5c0a1e59bb8260577f3ddcb132c1efe5cf39
-=======
-      <h3>Yo Dima.</h3>
-      
->>>>>>> resetProject
     </div>);
   }
 }
