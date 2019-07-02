d5ae94e52fb7bc54e4674c03a56569b4a666ccd5
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-07-02 03:10:48 -0400

    Reset done! We have all necessory dependencies to start the project.

---
+++ contracts/T_b.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+contract t_b {
+    mapping (address => uint256) tipjars;
+    
+    function tip(address _channel) public payable {
+        tipjars[_channel] = msg.value;
+    }
+    
+    function checkBalance() public view returns(uint256) {
+        return tipjars[msg.sender];
+    }
+    
+    function withdraw() public {
+        require(tipjars[msg.sender] > 0);
+        uint256 amount = tipjars[msg.sender];
+        tipjars[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+    }
+}
