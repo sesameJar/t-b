010640d8ec30298f9a7385deeaef075eafe0cca1
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-07-03 15:13:37 -0400

    Minor cleanup

--- contracts/T_b.sol
+++ contracts/T_b.sol
@@ -5,19 +5,19 @@ contract T_b {
     event withdrawTips(address channelOwner, uint256 amount);
     // Address is the channel owner's public address
     mapping (address => uint256) tipjars;
-    
+
     function tip(address _channel) public payable {
-        require(msg.value > 0 , "NO value."); // wastes some gas, we also check it on the front end
+        require(msg.value > 0, "NO value."); // wastes some gas, we also check it on the front end
         tipjars[_channel] += msg.value;
         emit tipMade(msg.sender, _channel, msg.value);
     }
-    
+
     function checkBalance() public view returns(uint256) {
         return tipjars[msg.sender];
     }
-    
+
     function withdraw() public {
-        require(tipjars[msg.sender] > 0);
+        require(tipjars[msg.sender] > 0, "The tipjar is empty");
         uint256 amount = tipjars[msg.sender];
         tipjars[msg.sender] -= amount;
         msg.sender.transfer(amount);
