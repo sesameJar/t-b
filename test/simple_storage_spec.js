02979e9662aa100cf4a51a06d1146e57d24aa160
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-07-03 13:43:11 -0400

    starting the contract testing

--- test/simple_storage_spec.js
+++ test/simple_storage_spec.js
@@ -1,5 +1,5 @@
 /*global contract, config, it, assert*/
-const SimpleStorage = require('Embark/contracts/SimpleStorage');
+const T_b = require('Embark/contracts/T_b');
 
 let accounts;
 
@@ -12,30 +12,18 @@ config({
   //  ]
   //},
   contracts: {
-    "SimpleStorage": {
-      args: [100]
+    "T_b": {
     }
   }
 }, (_err, web3_accounts) => {
   accounts = web3_accounts
 });
 
-contract("SimpleStorage", function () {
-  this.timeout(0);
-
-  it("should set constructor value", async function () {
-    let result = await SimpleStorage.methods.storedData().call();
-    assert.strictEqual(parseInt(result, 10), 100);
-  });
-
-  it("set storage value", async function () {
-    await SimpleStorage.methods.set(150).send();
-    let result = await SimpleStorage.methods.get().call();
-    assert.strictEqual(parseInt(result, 10), 150);
-  });
-
-  it("should have account with balance", async function() {
-    let balance = await web3.eth.getBalance(accounts[0]);
-    assert.ok(parseInt(balance, 10) > 0);
-  });
+contract("T_b", function () {
+ it("contract has deployed", async ()=> {
+   let address = await T_b.options.address
+   assert.ok(address)
+ })
+ it("")
+  
 });
