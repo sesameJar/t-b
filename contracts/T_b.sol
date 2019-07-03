829a940295cf6adcb9bec6b22e5db99714d2b9bf
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-07-03 13:20:53 -0400

    Contract updated

--- contracts/T_b.sol
+++ contracts/T_b.sol
@@ -1,10 +1,15 @@
 pragma solidity ^0.5.0;
 
 contract t_b {
+    event tipMade(address tipper, address channel, uint256 amount);
+    event withdrawTips(address channelOwner, uint256 amount);
+    // Address is the channel owner's public address
     mapping (address => uint256) tipjars;
     
     function tip(address _channel) public payable {
-        tipjars[_channel] = msg.value;
+        require(msg.value > 0 , "NO value."); // wastes some gas, we also check it on the front end
+        tipjars[_channel] += msg.value;
+        emit tipMade(msg.sender, _channel, msg.value);
     }
     
     function checkBalance() public view returns(uint256) {
@@ -16,5 +21,6 @@ contract t_b {
         uint256 amount = tipjars[msg.sender];
         tipjars[msg.sender] -= amount;
         msg.sender.transfer(amount);
+        emit withdrawTips(msg.sender, amount);
     }
 }
