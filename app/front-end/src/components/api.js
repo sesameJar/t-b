4f6b5388694e8a67eca4ae5f1bf08e7294fe1b54
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-07-04 17:39:43 -0400

    Connected frontend to firebase.

--- app/front-end/src/components/api.js
+++ app/front-end/src/components/api.js
@@ -1,30 +1,12 @@
-// import config from '../config/config'
+import config from '../config/config'
 
-// Just for testing
-const data = {
-  "videos" : [ {
-    "date" : 1562265077596,
-    "publicKey" : "0xCE79dB57f83cBdc29062fc636753715148868214",
-    "tips" : 0,
-    "title" : "King Gizzard and the Lizard Wizard",
-    "youtubeLink" : "https://www.youtube.com/watch?v=cmAlQvDQGoI"
-  }, {
-    "date" : 1562260247159,
-    "publicKey" : "0x06854f66338A7F9FD597E790A12F9E930D4Bd88B",
-    "tips" : 0,
-    "title" : "Chernobyl",
-    "youtubeLink" : "https://www.youtube.com/watch?v=FfDa8tR25dk"
-  }, {
-    "date" : 1562260247150,
-    "publicKey" : "0x06854f66338A7F9FD597E790A12F9E930D4Bd88B",
-    "tips" : 0,
-    "title" : "Chernobyl 2",
-    "youtubeLink" : "https://www.youtube.com/watch?v=jfY3RBzi6CI"
-  } ]
+const getVideos = async () => {
+  const response = await fetch(`${config.development.backendUrl}/getVideos`, {
+    method: "get",
+    mode: "cors",
+  })
+  let json = await response.json()
+  return json
 }
 
-const getAllVideos = async () => {
-  return data
-}
-
-export default getAllVideos
+export default getVideos
