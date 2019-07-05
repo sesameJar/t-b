2cd5f64ad7475449c325354df20bf084a959b7ac
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-07-05 13:16:51 -0400

    Changed getVideos function to return a list of objects containing a primary key, youtube link, and title.

--- app/back-end/functions/db/videos/index.js
+++ app/back-end/functions/db/videos/index.js
@@ -1,25 +1,19 @@
 exports.getVideos = async (databse) => {
     const videos = await databse.ref('Videos').once('value') //mind that Videos is capital
+    let responseArr = []
 
-    let reponseArr = []
-    videos.forEach(item => {
-        reponseArr.push(item.val())
-    })
-    console.log('videos : ', videos.val())
-    return reponseArr
-}
-
-let errorHandler = err => {
-    if(err) {
-        console.log('error : ' + err)
+    for (let [key, value] of Object.entries(videos.val())) {
+        responseArr.push({
+            primaryKey: key,
+            youtubeLink: value.youtubeLink,
+            title: value.title
+        })
     }
-    else {
-        console.log(success)
-    }
+    return responseArr
 }
 
 exports.addVideo = async (databse, data) => {
-   let newPost = await databse.ref('Videos').push(data)    
-   console.log("newPost" + newPost.key)
-   return newPost.key
+    let newPost = await databse.ref('Videos').push(data)
+    console.log("newPost" + newPost.key)
+    return newPost.key
 }
