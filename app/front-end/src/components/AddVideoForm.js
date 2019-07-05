f264cf46a49c2481bda592221aed7cea53772306
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-07-05 19:22:24 -0400

    Posting a new video adds it to the page.

--- app/front-end/src/components/AddVideoForm.js
+++ app/front-end/src/components/AddVideoForm.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react'
-import { addVideo } from './api'
 
 class AddVideoForm extends Component {
-  submitVideo = async (e) => {
-    e.preventDefault();
-    let data = {
-      youtubeLink: e.target.youtubeLink.value,
-      title: e.target.title.value,
-    }
-    await window.ethereum.enable()
-    let publicKey = window.web3.eth.accounts[0]
-    data.publicKey = publicKey
-    await addVideo(data)
-  }
+
   render() {
     if (!window.web3) {
       return (
@@ -20,7 +9,7 @@ class AddVideoForm extends Component {
       )
     } else {
       return (
-        <form onSubmit={this.submitVideo.bind(this)}>
+        <form onSubmit={this.props.submitVideo}>
           <input type="text" name="youtubeLink" placeholder="Youtube Link..." />
           <input type="text" name="title" placeholder="Video Title..." />
           <button>Submit</button>
