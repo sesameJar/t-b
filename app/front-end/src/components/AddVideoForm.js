b259d42d31f004d66a5307f671e8dd3c7c1315c4
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-07-05 18:16:51 -0400

    Added metamask is required message

--- app/front-end/src/components/AddVideoForm.js
+++ app/front-end/src/components/AddVideoForm.js
@@ -1,35 +1,33 @@
-import React, {Component} from 'react'
-import {addVideo} from './api'
-
+import React, { Component } from 'react'
+import { addVideo } from './api'
 
 class AddVideoForm extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            
-        }
+  submitVideo = async (e) => {
+    e.preventDefault();
+    let data = {
+      youtubeLink: e.target.youtubeLink.value,
+      title: e.target.title.value,
     }
-    submitVideo = async (e) => {
-        e.preventDefault();
-        let data = {
-            youtubeLink : e.target.youtubeLink.value,
-            title :  e.target.title.value,
-        
-        }
-        await window.ethereum.enable()
-        let publicKey = window.web3.eth.accounts[0]
-        //console.log(publicKey)
-        data.publicKey = publicKey
-        let primaryKey =await addVideo(data)
-        //console.log(primaryKey)
+    await window.ethereum.enable()
+    let publicKey = window.web3.eth.accounts[0]
+    data.publicKey = publicKey
+    await addVideo(data)
+  }
+  render() {
+    if (!window.web3) {
+      return (
+        <div>Metamask is required to post videos and send tips.</div>
+      )
+    } else {
+      return (
+        <form onSubmit={this.submitVideo.bind(this)}>
+          <input type="text" name="youtubeLink" placeholder="Youtube Link..." />
+          <input type="text" name="title" placeholder="Video Title..." />
+          <button>Submit</button>
+        </form>
+      )
     }
-    render(){
-       return(<form onSubmit={this.submitVideo.bind(this)}>
-           <input type="text" name="youtubeLink" placeholder="Youtube Link..." />
-           <input type="text" name="title" placeholder="Video Title..." />
-           <button>Submit</button>          
-       </form>) 
-    }
+  }
 }
 
 export default AddVideoForm;
