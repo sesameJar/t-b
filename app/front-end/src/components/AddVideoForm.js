2460b22cd6c5a59ca7ff32088faa65be74961d81
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-07-05 16:30:48 -0400

    add video component finished

--- app/front-end/src/components/AddVideoForm.js
+++ app/front-end/src/components/AddVideoForm.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react'
+import {addVideo} from './api'
+
 
 class AddVideoForm extends Component {
-    constructor(){
-        super()
-        this.state = {}
+    constructor(props){
+        super(props)
+        this.state = {
+            
+        }
     }
-
     submitVideo = async (e) => {
         e.preventDefault();
-        // let title = e.target.youtubeLink
+        let data = {
+            youtubeLink : e.target.youtubeLink.value,
+            title :  e.target.title.value,
+        
+        }
+        await window.ethereum.enable()
+        let publicKey = window.web3.eth.accounts[0]
+        //console.log(publicKey)
+        data.publicKey = publicKey
+        let primaryKey =await addVideo(data)
+        //console.log(primaryKey)
     }
-
     render(){
-       return(<form method="post" onSubmit={this.submitVideo}>
+       return(<form onSubmit={this.submitVideo.bind(this)}>
            <input type="text" name="youtubeLink" placeholder="Youtube Link..." />
            <input type="text" name="title" placeholder="Video Title..." />
-           <input type="text" name="publicKey" disabled /> 
            <button>Submit</button>          
        </form>) 
     }
--- app/front-end/src/components/api.js
+++ app/front-end/src/components/api.js
@@ -1,21 +1,37 @@
-import config from '../config/config'
+import config from "../config/config";
 
 const getVideos = async () => {
   const response = await fetch(`${config.development.backendUrl}/getVideos`, {
     method: "get",
-    mode: "cors",
-  })
-  let json = await response.json()
-  return json
-}
+    mode: "cors"
+  });
+  let json = await response.json();
+  return json;
+};
 
 const getVideo = async () => {
   const response = await fetch(`${config.development.backendUrl}/getVideo`, {
-    method : "get",
-    mode : "cors"
-  })
-  let json = await response.json()
-  return json
-}
+    method: "get",
+    mode: "cors"
+  });
+  let json = await response.json();
+  return json;
+};
 
-export {getVideos, getVideo}
+const addVideo = async data => {
+  console.log("api data", data);
+  const response = await fetch(`${config.development.backendUrl}/addVideo`, {
+    method: "post",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data)
+  });
+
+  let primaryKey = await response.json();
+  return primaryKey;
+};
+
+export { getVideos, getVideo, addVideo };
