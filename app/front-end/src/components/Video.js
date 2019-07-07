42fa8fcdedde9ab739c717f2099c7de1c044e809
Mehrad <mehrad.kavian@gmail.com>
2019-07-07 01:52:50 -0400

    video component updated (#8)

--- app/front-end/src/components/Video.js
+++ app/front-end/src/components/Video.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 import youtubeParser from './util'
+import {Link} from 'react-router-dom'
 
 
 class Video extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          
+          link : `/video/${this.props.prKey}`
         };
     }
 
@@ -14,10 +15,10 @@ class Video extends Component {
         let thumbUrl = `https://img.youtube.com/vi/${youtubeParser(this.props.link)}/default.jpg`
         return (
             <figure>
-              <a href={this.props.link}>
+              <Link to={this.state.link}>
                 <img src={thumbUrl} alt={this.props.title} />
                 <figcaption>{this.props.title}</figcaption>
-              </a>
+              </Link>
             </figure>
         )
     }
